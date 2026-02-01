cbafea70e16ced688e5aa38ac03d08f8b22362e4
Yakuphan-world <=>
2026-02-01 16:35:41 +0100

    n8n

---
+++ GitOps-with-Kubernetes,-Terraform,-GitLab,-FluxCD-on-AWS-EKS/EKS-TF/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS Region"
+  default     = "eu-central-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS Cluster"
+  default     = "EKS_CLOUD"
+}
